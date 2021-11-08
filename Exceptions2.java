326d4815bbb54bd574022ac04ad7d247fc455318
chakri8556 <chakri8556@gmail.com>
2021-11-08 22:05:30 +0530

    pushing exception handling assignment to git hub

---
+++ Exceptions2.java
@@ -0,0 +1,29 @@
+public class UnsupportedOperationExcepion extends Throwable {
+
+	public String toString() {
+		return "UnsupportedOperationExcepion";
+	}
+	
+
+}
+
+
+import java.util.Scanner;
+
+public class Exception2 {
+
+	public static void main(String[] args) throws UnsupportedOperationExcepion {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the Numerator");
+		int num = scanner.nextInt();
+		System.out.println("Enter the denominator");
+		int den = scanner.nextInt();
+		if(den==0) {
+			throw new UnsupportedOperationExcepion(); 
+		}
+		int c=num/den;
+		System.out.println(c);
+
+	}
+
+}
